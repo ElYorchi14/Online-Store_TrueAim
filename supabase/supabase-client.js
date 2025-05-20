8327574bf3457906d8b90154c6f08f6135fa1a0d
Jorge Palacios <cdmx2637@amerike.edu.mx>
2025-05-20 02:17:44 -0600

    Agregando la url y key del proyecto de supabase

--- supabase/supabase-client.js
+++ supabase/supabase-client.js
@@ -1,7 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // Reemplaza con tu URL y clave pública de tu proyecto Supabase
-const supabaseUrl = "https://TU_URL_DE_SUPABASE.supabase.co";
-const supabaseKey = "TU_CLAVE_PUBLICA_ANON";
+const supabaseUrl = "https://lqnimhwbcfplyfnfmbzr.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxxbmltaHdiY2ZwbHlmbmZtYnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NjA0NzUsImV4cCI6MjA2MTUzNjQ3NX0.4kUfbTEDWCnS5mdyD8Lsv8Ker8FuuWeWirA3x22hGuA";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
